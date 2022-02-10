e091bdda323b965b677b3093a84708b84ec45da4
yura <yrdog9494@gmail.com>
2022-02-10 16:33:20 +0900

    regex proto

--- src/main/java/com/dulgi/helper/common/Regex.java
+++ src/main/java/com/dulgi/helper/common/Regex.java
@@ -2,17 +2,19 @@ package com.dulgi.helper.common;
 
 public enum Regex {
     SETTER("^set[A-Z].*"),
-    GETTER("^get[A-Z].*");
+    GETTER("^get[A-Z].*"),
+    CAMEL("^[A-Z]+[a-Z][0-9]*"),
+    SNAKE("^[a-z]+[_][a-z]"),
+    PROPERTY("^[a-z]+[.][a-z]");
 
     public final String regex;
 
-    private Regex(String regex) { 
+    Regex(String regex) {
         this.regex = regex;
     }
     
     public String getRegex(){
         return regex;
     }
-
 
 }
